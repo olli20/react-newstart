193d64cfe82a474ed2dd28075a30539dfea4725a
Olena Voina <105532756+olli20@users.noreply.github.com>
2023-01-10 18:50:08 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import ReactDOM from 'react-dom';
 
-const element = React.createElement('div', { a: 5, b: 10, children: 'hello world' });
+const data = {
+  "id": "id-1",
+  "url": "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
+  "title": "Feathers. Art abstract",
+  "price": 500,
+  "author": {
+    "tag": "ractapopulous",
+    "url": "https://pixabay.com/users/ractapopulous-24766/"
+  },
+  "quantity": 10
+};
 
-console.log(element);
+const painting = (
+  <div>
+    <img src={data.url} alt={data.title} width="480" />
+    <h2>{data.title}</h2>
+    <p>
+      Author: <a href={data.author.url}>{data.author.tag}</a>
+    </p>
+    <p>Prise: {data.price} credits</p>
+    <p>Aviability</p>
+    <button type="button">Add to bascket</button>
+  </div>
+);
+
+ReactDOM.render(painting, document.querySelector('#root'));
+
+//JSX
+
+// const elem1 = <span>Hello</span>;
+// const elem2 = <span>JSX</span>;
+
+// const element = React.createElement('div', { a: 5, b: 10, children: 'Hello world' });
+
+// const jsxElement = <div>
+//   {elem1}
+//   {elem2}
+// </div>;
+
+// console.log(jsxElement);
+
+// ReactDOM.render(jsxElement, document.querySelector('#root'));
 
 
 // import ReactDOM from 'react-dom/client';
